2847057705c4f651d3a4f4c9b92c9916982de891
RiyadMia <96831477+RiyadMia@users.noreply.github.com>
2022-05-02 12:25:30 +0600

    mongo db conected

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -9,9 +9,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-async function run() {}
+// async function run() {}
 
-run().catch(console.dir);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kducj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  console.log("server connected");
+  // perform actions on the collection object
+  client.close();
+});
+
+//
+// run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Running Mobile  Server");
